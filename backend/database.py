6d35126af7bfd4da27444b1f0d7a48237855119b
Yashavanth H S <108397456+VIRTUOSOCRYPTO@users.noreply.github.com>
2025-10-03 17:01:02 +0530

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -22,7 +22,7 @@ def clean_mongo_doc(doc):
 
 # MongoDB connection
 mongo_url = os.environ.get('MONGO_URL', 'mongodb://localhost:27017')
-db_name = os.environ.get('DB_NAME', 'earnwise_production')
+db_name = os.environ.get('DB_NAME', 'earnaura_production')
 
 client = AsyncIOMotorClient(mongo_url)
 db = client[db_name]
