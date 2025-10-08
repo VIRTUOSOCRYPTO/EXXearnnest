035e96dcc44aef46ae03a5485ecade1431bfb63b
Yashavanth H S <108397456+VIRTUOSOCRYPTO@users.noreply.github.com>
2025-10-08 17:33:39 +0530

    Update RealTimeNotifications.js

--- frontend/src/components/RealTimeNotifications.js
+++ frontend/src/components/RealTimeNotifications.js
@@ -214,7 +214,7 @@ const RealTimeNotifications = () => {
       </div>
 
       {/* Toast Notifications */}
-      <div className="fixed top-4 right-4 z-50 space-y-2">
+      <div className="fixed top-4 right-4 z-[70] space-y-2">
         {toasts.map((toast) => {
           const IconComponent = getNotificationIcon(toast.type);
           
