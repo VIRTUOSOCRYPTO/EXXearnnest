3ccc6b08e8fdb6dfd4908fc5a4fc7f59ea5469e3
Yashavanth H S <108397456+VIRTUOSOCRYPTO@users.noreply.github.com>
2025-10-22 18:27:26 +0530

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,10 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import ErrorBoundary from "./components/ErrorBoundary";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <ErrorBoundary showDetails={process.env.NODE_ENV === 'development'}>
+      <App />
+    </ErrorBoundary>
   </React.StrictMode>,
 );
